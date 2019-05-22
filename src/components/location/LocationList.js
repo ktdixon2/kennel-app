a22ad60551c30a913873ff07346d28448e7bf0fb
System Administrator <root@Kenneths-MacBook-Pro.local>
2019-05-22 13:56:33 -0500

    first commit

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class LocationList extends Component {
+    render() {
+        return (
+            <section className="locations">
+            {
+                this.props.locations.map(location =>
+                    <div key={location.id}>
+                        {location.name}
+                        {location.address}
+                    </div>
+                )
+            }
+            </section>
+        )
+    }
+}
+
+export default LocationList
